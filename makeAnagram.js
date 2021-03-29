a758b17b115cf1175c919c3cb19caf305a983a35
Lita Nurlaelati <litanurlaelati@gmail.com>
2021-03-29 14:24:53 +0700

    Update makeAnagram.js
    Sample test case 0, 1, 2 berhasil. Tapi test case (real) gagal

--- makeAnagram.js
+++ makeAnagram.js
@@ -1,4 +1,6 @@
-# Link soal: https://www.hackerrank.com/challenges/ctci-making-anagrams/problem
+// Link soal: https://www.hackerrank.com/challenges/ctci-making-anagrams/problem
+
+let indices = []
 
 function makeAnagram(a, b) {
   /**
@@ -12,14 +14,15 @@ function makeAnagram(a, b) {
    * 6. Jumlahkan hasil no 3 & 5
    */
 
-  const arrayA = a.split("").sort()
-  const arrayB = b.split("").sort()
+  const arrayA = a.split('').sort()
+  const arrayB = b.split('').sort()
   let banding
   let hasilBanding = []
   let hitung = 0
+  let x
 
-  // console.log(arrayA)
-  // console.log(arrayB)
+  //console.log(arrayA)
+  //console.log(arrayB)
   // langkah no 1
   for(let i = 0; i < arrayA.length; i++) {
     banding = arrayB.indexOf(arrayA[i])
@@ -28,15 +31,51 @@ function makeAnagram(a, b) {
     }
   }
 
-  // console.log(hasilBanding)
+  //console.log(hasilBanding)
+  // remove duplikat
+  let arrayBanding = [...new Set(hasilBanding)]
+  //console.log(arrayBanding)
+
+  for(let j = 0; j < arrayBanding.length; j++) {
+    check(arrayA, arrayBanding[j])
+  }
+
+  //console.log(indices)
+
+  indices = []
+
+  for(let j = 0; j < arrayBanding.length; j++) {
+    check(arrayA, arrayBanding[j])
+    let p = indices.length
+    //console.log(p)
+    indices = []
+    check(arrayB, arrayBanding[j])
+    let q = indices.length
+    //console.log(p)
+    indices = []
+
+    if (p !== q) hitung++
+  }
+
+  //console.log(indices)
   // langkah no 2 - 6
   for(let j = 0; j < arrayA.length; j++) {
-    if ((hasilBanding.indexOf(arrayA[j])) === -1) hitung++
+    if ((arrayBanding.indexOf(arrayA[j])) === -1) hitung++
   }
 
   for(let k = 0; k < arrayB.length; k++) {
-    if ((hasilBanding.indexOf(arrayB[k])) === -1) hitung++
+    if ((arrayBanding.indexOf(arrayB[k])) === -1) hitung++
   }
 
   return hitung
 }
+
+function check(array, element) {
+  let idx = array.indexOf(element);
+  while (idx !== -1) {
+    indices.push(idx);
+    idx = array.indexOf(element, idx + 1);
+  }
+}
+
+console.log(makeAnagram("fcrxzwscanmligyxyvym", "jxwtrhvujlmrpdoqbisbwhmgpmeoke"))
